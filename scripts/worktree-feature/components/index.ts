4d1f5c3d9feaaaf49802e7370633aca7dab43ec8
William Cory <willcory10@gmail.com>
2026-04-11 23:54:52 -0700

    🗑️ remove: worktree-feature scripts

--- scripts/worktree-feature/components/index.ts
+++
@@ -1,9 +0,0 @@
-export { Discover } from "./Discover";
-export { Implement } from "./Implement";
-export { Validate } from "./Validate";
-export { Review } from "./Review";
-export { ReviewFix } from "./ReviewFix";
-export { Report } from "./Report";
-export { ValidationLoop } from "./ValidationLoop";
-export { TicketPipeline } from "./TicketPipeline";
-export type { Ticket } from "./Discover.schema";
