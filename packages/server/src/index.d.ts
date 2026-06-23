dde752f9910f92e19539c9d07a5554ab74bb67c4
William Cory <willcory10@gmail.com>
2026-06-23 15:21:14 -0400

    ✅ kanban: smithers/cov-46-no-test-guards-ui-workflows-gateway-mounts-ui-fi.md

--- packages/server/src/index.d.ts
+++ packages/server/src/index.d.ts
@@ -1131,6 +1131,11 @@ declare class Gateway {
     /**
    * @param {IncomingMessage} req
    * @param {ServerResponse} res
+   */
+    handleElectricWrite(req: IncomingMessage, res: ServerResponse$1): Promise<void>;
+    /**
+   * @param {IncomingMessage} req
+   * @param {ServerResponse} res
    * @param {string} [forcedMethod]
    */
     handleHttpRpc(req: IncomingMessage, res: ServerResponse$1, forcedMethod?: string): Promise<void>;
