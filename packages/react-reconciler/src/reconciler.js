89c04527e9c429e54ee72139b21db2a9ec5c075e
William Cory <willcory10@gmail.com>
2026-07-29 15:41:03 -0700

    🔖 chore(release): 0.32.0

--- packages/react-reconciler/src/reconciler.js
+++ packages/react-reconciler/src/reconciler.js
@@ -435,7 +435,7 @@ if (!hookHost.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
 }
 const injectedDevToolsConfig = {
   bundleType: typeof process !== "undefined" && process.env.NODE_ENV === "production" ? 0 : 1,
-  version: "0.31.1",
+  version: "0.32.0",
   rendererPackageName: "@smithers-orchestrator/react-reconciler",
   findFiberByHostInstance: () => null,
 };
