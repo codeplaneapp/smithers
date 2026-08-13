6525da9a15c5177c181080aa584fc2518e131cd9
William Cory <fucory@proton.me>
2026-08-12 18:36:19 -0700

    0.34.0

--- packages/react-reconciler/src/reconciler.js
+++ packages/react-reconciler/src/reconciler.js
@@ -435,7 +435,7 @@ if (!hookHost.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
 }
 const injectedDevToolsConfig = {
   bundleType: typeof process !== "undefined" && process.env.NODE_ENV === "production" ? 0 : 1,
-  version: "0.33.1",
+  version: "0.34.0",
   rendererPackageName: "@smthrs/react-reconciler",
   findFiberByHostInstance: () => null,
 };
